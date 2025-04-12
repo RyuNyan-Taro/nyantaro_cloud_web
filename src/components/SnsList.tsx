86999bbedf20a9c0cc240ffb6301eaa54af6593b
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-04-12 15:31:00 +0900

    doing to add x link with scroll.

--- src/components/SnsList.tsx
+++ src/components/SnsList.tsx
@@ -1,26 +1,39 @@
-export default function SnsList () {
+'use client'
+
+import { useEffect } from 'react'
+
+export default function SnsList() {
+    useEffect(() => {
+        const script = document.createElement('script')
+        script.src = 'https://platform.twitter.com/widgets.js'
+        script.async = true
+        document.body.appendChild(script)
+    }, [])
+
     return (
-        <div>
-            <h2>
-                SNS
-            </h2>
-            <p>
-                <a 
-                    href="https://x.com/Nyantar83781816" 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                    className="link"
+        <div className="h-full overflow-y-auto p-4 space-y-4">
+            <h2 className="text-2xl font-bold">SNS</h2>
+
+            {/* Twitterタイムライン */}
+            <div className="h-[500px]">
+                <a
+                    className="twitter-timeline"
+                    data-height="500"
+                    href="https://twitter.com/Nyantar83781816"
                 >
-                X(twitter)</a><br />
-                <a 
-                    href="https://www.instagram.com/nyantarotaro/" 
-                    target="_blank" 
-                    rel="noopener noreferrer"
-                    className="link"
-                >
-                instagram
+                    Tweets by Nyantar83781816
                 </a>
-            </p>
+            </div>
+
+            {/* Instagramリンク */}
+            <a
+                href="https://www.instagram.com/nyantarotaro/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 underline block"
+            >
+                Instagram
+            </a>
         </div>
-    );
+    )
 }
