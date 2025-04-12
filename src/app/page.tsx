888ea42bc5c83b89336b87ff1f637db1921d540d
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-04-12 13:09:04 +0900

    revert to check tailwindcss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,51 +2,44 @@ import './page.css'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-indigo-500 to-pink-500 text-white">
-      <h1 className="text-4xl font-bold">Tailwind is working! 🚀</h1>
-    </main>
-  )
+    <div>
+      <h1>
+        Nyantaro Cloud
+      </h1>
+      <div className="container">
+        <div className="left-scroll">
+          <h2>
+            Contents
+          </h2>
+          <p>
+            blog<br />
+            galley<br />
+            movie
+          </p>
+        </div>
+        <div className="right-fixed">
+          <h2>
+            SNS
+          </h2>
+          <p>
+            <a 
+              href="https://x.com/Nyantar83781816" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="link"
+            >
+              X(twitter)</a><br />
+            <a 
+              href="https://www.instagram.com/nyantarotaro/" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className="link"
+            >
+              instagram
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
-// export default function Home() {
-//   return (
-//     <div>
-//       <h1>
-//         Nyantaro Cloud
-//       </h1>
-//       <div className="container">
-//         <div className="left-scroll">
-//           <h2>
-//             Contents
-//           </h2>
-//           <p>
-//             blog<br />
-//             galley<br />
-//             movie
-//           </p>
-//         </div>
-//         <div className="right-fixed">
-//           <h2>
-//             SNS
-//           </h2>
-//           <p>
-//             <a 
-//               href="https://x.com/Nyantar83781816" 
-//               target="_blank" 
-//               rel="noopener noreferrer"
-//               className="link"
-//             >
-//               X(twitter)</a><br />
-//             <a 
-//               href="https://www.instagram.com/nyantarotaro/" 
-//               target="_blank" 
-//               rel="noopener noreferrer"
-//               className="link"
-//             >
-//               instagram
-//             </a>
-//           </p>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
